07b5188751da56d61dc2f1b9c40fca79c3ca586a
Yevhen Grinman <yevgirnman@yahoo.com>
2018-01-26 17:50:16 -0500

    Tests and templates added

--- users-service/manage.py
+++ users-service/manage.py
@@ -1,7 +1,18 @@
 from flask_script import Manager
 from project import create_app, db
 import unittest
+import coverage
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*'
+    ]
+)
+COV.start()
+
 
 app = create_app()
 manager = Manager(app)
@@ -21,6 +32,48 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@manager.command
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='yevhen', email='yevhen@realpython.com'))
+    db.session.add(User(username='yevhengrinman', email='yevhen.grinman@gmail.com'))
+    db.session.commit()
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__=='__main__':
     manager.run()
